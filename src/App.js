2dc1b5db38fc7e010d9bff89bb1ede236bf64ad6
fabiank0 <kuefferfabian@gmail.com>
2021-03-31 15:05:38 +0200

    changed env API URL

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import logo from './logo.png';
 import './App.css';
 import Api from './lib/Http/Api';
-import {useState} from 'react';
+import { useState } from 'react';
+import Log from './helper/Log.js';
 
 function App() {
-    const [listings, setListings] = useState([]);
-    const loadListings = async () => {
-        const response = await Api.get('/listing');
-        setListings(response.data);
-    }
+  const Logger = new Log('App.js');
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Welcome to BnB Explorer
-                </p>
-                <button onClick={loadListings} style={{fontSize: '28px'}}>GET /listings</button>
-                <div>
-                    {
-                        listings.map((listing) =>
-                          <p>{listing}</p>
-                        )
-                    }
-                </div>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-            </header>
+  const [listings, setListings] = useState([]);
+
+  const loadListings = async () => {
+    const response = await Api.get('listings');
+    Logger.log(response, 'res');
+    setListings(response.data);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to BnB Explorer</p>
+        <button onClick={loadListings} style={{ fontSize: '28px' }}>
+          GET /listings
+        </button>
+        <div>
+          {listings.map((listing) => {
+            return <p key={listing.id}>{JSON.stringify(listing)}</p>;
+          })}
         </div>
-    );
+      </header>
+    </div>
+  );
 }
 
 export default App;
