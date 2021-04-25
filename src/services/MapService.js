8bf931dc6922e617a48a1ac45576768439ec379b
PKiechl <pascal.kiechl@bluewin.ch>
2021-04-25 10:31:50 +0200

    introduced MapService, reorganized directories.

---
+++ src/services/MapService.js
@@ -0,0 +1,29 @@
+export const getMappedCrimeData = (crimeData) => {
+  return typeof crimeData !== 'undefined' && crimeData //verbose checking to make SonarQube happy ...
+    ? {
+        positions: crimeData.map((e) => {
+          return { lat: e.latitude, lng: e.longitude };
+        }),
+        options: {
+          radius: 20,
+          opacity: 0.6,
+          gradient: ['rgba(255,255,0,0)', 'rgba(255,255,0,1)'],
+        },
+      }
+    : [];
+};
+
+export const getMappedCleanlinessData = (cleanlinessData) => {
+  return typeof cleanlinessData !== 'undefined' && cleanlinessData //verbose checking to make SonarQube happy ...
+    ? {
+        positions: cleanlinessData.map((e) => {
+          return { lat: e.latitude, lng: e.longitude };
+        }),
+        options: {
+          radius: 20,
+          opacity: 0.6,
+          gradient: ['rgba(0,0,255,0)', 'rgba(0, 0, 255, 1)'],
+        },
+      }
+    : [];
+};
