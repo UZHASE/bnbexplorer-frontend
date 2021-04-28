57f51725329c8f887566390d5e1d54a73e089455
PKiechl <pascal.kiechl@bluewin.ch>
2021-04-28 16:13:14 +0200

    prettier strikes again

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -117,19 +117,18 @@ const Map = (props) => {
             style={{ height: '500px', paddingBottom: '50px' }}
             onGoogleApiLoaded={({ map, maps }) => apiHasLoaded(map, maps)}
           >
-            {listings
-              .map((e) => {
-                return (
-                  <AnyReactComponent
-                    lat={e.latitude}
-                    lng={e.longitude}
-                    text='My Marker'
-                    key={e.id}
-                    setListing={setListing}
-                    id={e.id}
-                  />
-                );
-              })}
+            {listings.map((e) => {
+              return (
+                <AnyReactComponent
+                  lat={e.latitude}
+                  lng={e.longitude}
+                  text='My Marker'
+                  key={e.id}
+                  setListing={setListing}
+                  id={e.id}
+                />
+              );
+            })}
             {places.map((e) => {
               return (
                 <AnyReactComponent
