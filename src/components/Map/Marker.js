2aa0623362fc29609ed9f317e16c1ebd61cdede4
PKiechl <pascal.kiechl@bluewin.ch>
2021-05-15 14:38:22 +0200

    hover box now placed dependent on corresponding marker's relative position on map. prevents hover box from clipping outside of parent container.

--- src/components/Map/Marker.js
+++ src/components/Map/Marker.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import RoomIcon from '@material-ui/icons/Room';
 import Container from '@material-ui/core/Box';
+import classNames from 'classnames';
 
-/*
-https://blaipratdesaba.com/how-to-use-an-npm-node-module-that-has-been-forked-b7dd522fdd08
-if when deployed the map does not work, need to add post install script
- */
+import './Marker.scss';
+import { getHoverBoxStyleData, composeId } from '../../services/markerService';
 
-const HoverBox = ({ place }) => {
-  // console.log('place', place);
-
-  /*
-  TODO: select parent marker by id, then check positioning in map element and determine
-    whether the hover element needs vertical/horizontal offset 
-   */
-  // const markerEl = document.getElementById(key);
-  // console.log(markerEl);
+const HoverBox = ({ place, placeId, isHidden, id }) => {
+  const { foldLeft, foldTop, markerOffsetTopComputed } = getHoverBoxStyleData(
+    isHidden,
+    placeId
+  );
 
   return (
     <Container
       maxWidth='false'
       fixed='true'
-      className={'current-container'}
+      id={id}
+      className={classNames({
+        'current-container': true,
+        'fold-left': foldLeft,
+        'fold-top': foldTop,
+      })}
       style={{
-        backgroundColor: 'rgba(255,255,255,1)',
-        borderRadius: '6px',
-        boxShadow: '0 2px 7px 1px rgb(0 0 0 / 30%)',
-        width: '190px',
-        maxHeight: '130px',
-        padding: '12px',
-        zIndex: '1000',
-        position: 'relative',
+        top: markerOffsetTopComputed,
       }}
     >
       <div
@@ -68,8 +61,7 @@ const HoverBox = ({ place }) => {
 };
 
 const AnyReactComponent = (props) => {
-  const { id, setListing, key, type } = props;
-  // const color = type && type === 'marker' ? 'primary' : 'secondary';
+  const { id, setListing, type, place } = props;
   const color = {
     marker: 'rgba(40,40,220,0.95)',
     listings: 'rgba(244,67,54,0.85)',
@@ -84,21 +76,46 @@ const AnyReactComponent = (props) => {
   };
 
   return (
-    <div
-      style={{ position: 'absolute', left: '-12px', top: '-12px' }}
-      key={key}
-    >
-      <RoomIcon
-        key={key}
+    <div>
+      <div
         style={{
-          position: 'relative',
-          cursor: 'pointer',
-          zIndex: type === 'marker' ? 400 : 'unset',
-          color: color[type],
+          position: 'absolute',
+          left: '-12px',
+          top: '-12px',
         }}
-        onClick={() => clickHandler(id)}
-      />
-      {props.$hover && type === 'marker' ? <HoverBox {...props} /> : null}
+        key={id}
+        id={id}
+      >
+        <RoomIcon
+          key={id}
+          style={{
+            position: 'relative',
+            cursor: 'pointer',
+            zIndex: type === 'marker' ? 400 : 'unset',
+            color: color[type],
+          }}
+          onClick={() => clickHandler(id)}
+        />
+        {props.$hover && type === 'marker' ? (
+          <HoverBox
+            place={place}
+            placeId={id}
+            isHidden={false}
+            id={composeId(id)}
+          />
+        ) : null}
+      </div>
+      <div className={'hidden'}>
+        {/*  /!*hidden hover-info render for height measurements*!/*/}
+        {type === 'marker' ? (
+          <HoverBox
+            place={place}
+            placeId={id}
+            id={composeId(id, true)}
+            isHidden={true}
+          />
+        ) : null}
+      </div>
     </div>
   );
 };
