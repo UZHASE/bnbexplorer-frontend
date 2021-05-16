05ae15f3facb0ad27612d467dd07074d61e09b36
PKiechl <pascal.kiechl@bluewin.ch>
2021-05-16 13:22:03 +0200

    WIP

--- src/__test__/Searchbar.test.js
+++ src/__test__/Searchbar.test.js
@@ -1,22 +1,63 @@
 import { render, screen } from '@testing-library/react';
+import { mount } from 'enzyme';
 import { configure } from '@testing-library/dom';
+import { act } from 'react-dom/test-utils';
 configure({ testIdAttribute: 'id' });
 // make actual Ids available as testIds
+import Searchbar from '../components/Searchbar';
+import { TextField } from '@material-ui/core';
+
+let propagatedValue = null;
+
+const searchBarProps = {
+  onSearchValueChange: (val) => {
+    propagatedValue = val;
+    console.log('received value from searchbar:', propagatedValue);
+  },
+};
+
+test('renders searchbar', () => {
+  render(<Searchbar />);
+  expect(screen.getByTestId('searchbar-component')).toBeInTheDocument();
+});
+
+test('renders searchbar with props', () => {
+  const wrapper = mount(<Searchbar {...searchBarProps} />);
+  console.log(wrapper.find(TextField).debug()); // TODO CLEANUP
+
+  const input = wrapper.find(TextField).find('input');
+  const event = { target: { value: 'test value' } };
+
+  act(() => {
+    input.prop('onChange').call(null, event);
+  });
+
+  wrapper.find(TextField).prop('onChange').call(null, event);
 
 
-// test('renders header', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText('BnB explorer');
-//   expect(linkElement).toBeInTheDocument();
-// });
+  console.log(wrapper.find(TextField).debug()); // TODO CLEANUP
 
-//
-// test('renders searchbar', () => {
-//   render(<App />);
-//   expect(screen.getByTestId('searchbar-component')).toBeInTheDocument();
-// });
-//
-// test('renders filterbox', () => {
-//   render(<App />);
-//   expect(screen.getByTestId('filterbox-component')).toBeInTheDocument();
-// });
+  // act(() => {
+  //   input.simulate('focus');
+  //   input.props().onChange({
+  //     target: { value: 'test value changed' },
+  //   });
+  //   input.simulate('change');
+  //   input.simulate('blur');
+  // });
+
+  // wrapper.find(TextField).
+  // input.simulate('focus');
+  // input.simulate('change', { target: { value: 'Changed Value' } });
+
+  // input.simulate('focus');
+
+  // render(<Searchbar {...searchBarProps} />);
+  // let searchBar = screen.getByTestId('searchbar-component');
+  // expect(searchBar).toBeInTheDocument();
+  //
+  // searchBar.props().onChange({ target: { value: 'changed value' } });
+  // searchBar.simulate('change');
+  //
+  // console.log('Searchbar:!!!!, after input', searchBar);
+});
