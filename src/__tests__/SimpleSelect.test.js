da67914bda79875e0127d0c52611610ff29b4605
PKiechl <pascal.kiechl@bluewin.ch>
2021-05-17 12:01:25 +0200

    additional condition coverage tests for SimpleSelect

--- src/__tests__/SimpleSelect.test.js
+++ src/__tests__/SimpleSelect.test.js
@@ -2,9 +2,29 @@ import { render, screen } from '@testing-library/react';
 import { configure } from '@testing-library/dom';
 configure({ testIdAttribute: 'id' });
 // make actual IDs available as testIds
-import SimpleSelect from '../components/FilterItems/SimpleSelect';
+import SimpleSelect, {
+  getStyles,
+} from '../components/FilterItems/SimpleSelect';
+import { useTheme } from '@material-ui/core/styles';
 
-// setup
+// mock components / constants
+const typographyFontWeightRegular = '400';
+const typographyFontWeightMedium = '500';
+
+const DummyComponent = ({ falseValue, values }) => {
+  const theme = useTheme();
+
+  return (
+    <div>
+        <span
+          id='dummy-component-span'
+          style={getStyles(falseValue, values, theme)}
+        >
+          dummy component text
+        </span>
+    </div>
+  );
+};
 
 // mock data
 const propagationHandler = () => {
@@ -24,4 +44,44 @@ test('SimpleSelect static render', () => {
   expect(
     screen.getByTestId(`multi-select-${simpleSelectProps.name}`)
   ).toBeInTheDocument();
+});
+
+test('SimpleSelect static render, no text', () => {
+  const temp = simpleSelectProps;
+  delete temp.text;
+  render(<SimpleSelect {...temp} />);
+  expect(screen.getByTestId(`multi-select-${temp.name}`)).toBeInTheDocument();
+});
+
+test('getStyles condition test, no index match found', () => {
+  // simulate case where value is not found in values, hence no index match
+  // easiest way to do with a fake react component, since not possible to test function
+  // by its own, will result in 'invalid hook call'
+  render(
+    <DummyComponent
+      falseValue={'value not in values'}
+      values={simpleSelectProps.values}
+    />
+  );
+
+  const spanElement = screen.getByTestId('dummy-component-span');
+  expect(spanElement).toBeInTheDocument();
+  const detectedFontWeight = spanElement.style.fontWeight;
+  expect(detectedFontWeight).toEqual(typographyFontWeightRegular);
+});
+
+test('getStyles condition test, has index match', () => {
+  // somewhat unnecessary, as already covered by static render, but for completeness
+  // a standalone test of the getStyles function
+  render(
+    <DummyComponent
+      falseValue={simpleSelectProps.values[0]}
+      values={simpleSelectProps.values}
+    />
+  );
+
+  const spanElement = screen.getByTestId('dummy-component-span');
+  expect(spanElement).toBeInTheDocument();
+  const detectedFontWeight = spanElement.style.fontWeight;
+  expect(detectedFontWeight).toEqual(typographyFontWeightMedium);
 });
