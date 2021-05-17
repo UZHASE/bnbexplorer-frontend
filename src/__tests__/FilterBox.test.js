d6789036c58e021c00da86d10d8074760e097049
fabiank0 <kuefferfabian@gmail.com>
2021-05-17 14:08:24 +0200

    wip filterboxtest

--- src/__tests__/FilterBox.test.js
+++ src/__tests__/FilterBox.test.js
@@ -2,6 +2,8 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { configure } from '@testing-library/dom';
 import FilterBox from '../components/FilterBox';
+import { mount, shallow } from 'enzyme';
+import Api from '../lib/Http/Api';
 
 configure({ testIdAttribute: 'id' });
 // make actual Ids available as testIds
@@ -11,3 +13,29 @@ test('No data renders CircularProgress', () => {
   const linkElement = screen.getByTestId('circularProgress-FilterBox');
   expect(linkElement).toBeInTheDocument();
 });
+
+test('filterbox changesettings', async () => {
+  jest.spyOn(Api, 'get').mockResolvedValueOnce({
+    data: res,
+  });
+
+  FilterBox.loadMetaListingData = jest.fn().mockResolvedValue({ data: res });
+
+  const wrapper = mount(<FilterBox />);
+  await wrapper.setProps();
+  await wrapper.update()
+  const p = wrapper
+});
+
+
+const res = {
+  maxListingsPerHost: 327,
+  maxPrice: 9999,
+  minListingsPerHost: 1,
+  minPrice: 0,
+  neighbourhoods: (5)[
+    ('Brooklyn', 'Manhattan', 'Queens', 'Staten Island', 'Bronx')
+    ],
+  numOfListings: 9705,
+  roomTypes: (3)[('Private room', 'Entire home/apt', 'Shared room')],
+};
