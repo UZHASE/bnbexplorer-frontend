3c574076b70e9aab652fb34d0c85af6a7f7de094
fabiank0 <kuefferfabian@gmail.com>
2021-05-22 11:09:49 +0200

    missing tests

--- src/__tests__/mapService.test.js
+++ src/__tests__/mapService.test.js
@@ -2,12 +2,21 @@ import { configure } from '@testing-library/dom';
 import {
   getMappedCrimeData,
   getMappedRodentData,
-  getMappedComplaintsData,
+  getMappedComplaintData,
 } from '../services/mapService';
 configure({ testIdAttribute: 'id' });
 // make actual Ids available as testIds
 
-test('Rodent data returns empty positions', () => {
+const mockData = [
+  {
+    latitude: 50.0,
+    longitude: 90.0,
+  },
+  { latitude: 10.0, longitude: 20.0 },
+];
+
+test('Empty Rodent data returns empty positions', () => {
+  //check that if rodent data was provided, but the array is empty, that the positions will be empty as well
   expect(getMappedRodentData([])).toEqual({
     options: {
       gradient: ['rgba(0,0,255,0)', 'rgba(0, 0, 255, 1)'],
@@ -19,10 +28,28 @@ test('Rodent data returns empty positions', () => {
 });
 
 test('No Rodent data returns empty object', () => {
+  //check that if no rodent data was provided from the backend,
+  // that the resulting object will be empty
   expect(getMappedRodentData()).toEqual({});
 });
 
-test('Crime data returns empty positions', () => {
+test('Rodent data positions mapping', () => {
+  //check that the mapping puts the position data into the correct format
+  expect(getMappedRodentData(mockData)).toEqual({
+    options: {
+      gradient: ['rgba(0,0,255,0)', 'rgba(0, 0, 255, 1)'],
+      opacity: 0.6,
+      radius: 20,
+    },
+    positions: [
+      { lat: 50.0, lng: 90.0 },
+      { lat: 10.0, lng: 20.0 },
+    ],
+  });
+});
+
+test('Empty Crime data returns empty positions', () => {
+  //check that if Crime data was provided, but the array is empty, that the positions will be empty as well
   expect(getMappedCrimeData([])).toEqual({
     positions: [],
     options: {
@@ -33,21 +60,56 @@ test('Crime data returns empty positions', () => {
   });
 });
 
+test('Crime data returns mapped positions', () => {
+  expect(getMappedCrimeData(mockData)).toEqual({
+    //check that the mapping puts the position data into the correct format
+    positions: [
+      { lat: 50.0, lng: 90.0 },
+      { lat: 10.0, lng: 20.0 },
+    ],
+    options: {
+      radius: 20,
+      opacity: 0.6,
+      gradient: ['rgba(255,255,0,0)', 'rgba(255,255,0,1)'],
+    },
+  });
+});
+
 test('No Crime data returns empty object', () => {
+  //check that if no crime data was provided from the backend,
+  // that the resulting object will be empty
   expect(getMappedCrimeData()).toEqual({});
 });
 
-test('Complaints data returns empty positions', () => {
-  expect(getMappedComplaintsData([])).toEqual({
+test('Empty Complaints data returns empty positions', () => {
+  //check that if Complaints data was provided, but the array is empty, that the positions will be empty as well
+  expect(getMappedComplaintData([])).toEqual({
     positions: [],
     options: {
       radius: 20,
       opacity: 0.6,
-      gradient: ['rgba(240,40,120,0)', 'rgba(240,140,120,1)'],
+      gradient: ['rgba(240, 40, 120, 0)', 'rgba(240, 40, 120, 1)'],
+    },
+  });
+});
+
+test('Complaints data returns mapped positions', () => {
+  //check that the mapping puts the position data into the correct format
+  expect(getMappedComplaintData(mockData)).toEqual({
+    positions: [
+      { lat: 50.0, lng: 90.0 },
+      { lat: 10.0, lng: 20.0 },
+    ],
+    options: {
+      radius: 20,
+      opacity: 0.6,
+      gradient: ['rgba(240, 40, 120, 0)', 'rgba(240, 40, 120, 1)'],
     },
   });
 });
 
 test('No Complaints data returns empty object', () => {
-  expect(getMappedComplaintsData()).toEqual({});
+  //check that if no complaints data was provided from the backend,
+  // that the resulting object will be empty
+  expect(getMappedComplaintData()).toEqual({});
 });
