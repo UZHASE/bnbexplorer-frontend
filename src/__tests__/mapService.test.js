1f779085758a5955a13526e8aa0fc498a759e19a
fabiank0 <kuefferfabian@gmail.com>
2021-05-22 10:52:41 +0200

    complaints test

--- src/__tests__/mapService.test.js
+++ src/__tests__/mapService.test.js
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react';
 import { configure } from '@testing-library/dom';
-import FilterBox from '../components/FilterBox';
-import { getMappedRodentData } from '../services/mapService';
+import {
+  getMappedCrimeData,
+  getMappedRodentData,
+  getMappedComplaintsData,
+} from '../services/mapService';
 configure({ testIdAttribute: 'id' });
 // make actual Ids available as testIds
 
@@ -18,4 +20,34 @@ test('Rodent data returns empty positions', () => {
 
 test('No Rodent data returns empty object', () => {
   expect(getMappedRodentData()).toEqual({});
+});
+
+test('Crime data returns empty positions', () => {
+  expect(getMappedCrimeData([])).toEqual({
+    positions: [],
+    options: {
+      radius: 20,
+      opacity: 0.6,
+      gradient: ['rgba(255,255,0,0)', 'rgba(255,255,0,1)'],
+    },
+  });
+});
+
+test('No Crime data returns empty object', () => {
+  expect(getMappedCrimeData()).toEqual({});
+});
+
+test('Complaints data returns empty positions', () => {
+  expect(getMappedComplaintsData([])).toEqual({
+    positions: [],
+    options: {
+      radius: 20,
+      opacity: 0.6,
+      gradient: ['rgba(240,40,120,0)', 'rgba(240,140,120,1)'],
+    },
+  });
+});
+
+test('No Complaints data returns empty object', () => {
+  expect(getMappedComplaintsData()).toEqual({});
 });
