d58bc3010b4e595bd38e8717a40705d0118ce6ca
PKiechl <PKiechl@users.noreply.github.com>
2021-05-27 20:47:56 +0000

    deploy: 035b93b04befde0626cf498a759ef1f17fc8e77b

--- entry.js
+++ entry.js
@@ -9,7 +9,7 @@
       import ReactDOM from "react-dom";
 
 
-      import ReactWrapper from '../../../_actions/fabiank0/jsdoc-action/master/node_modules/better-docs/lib/react-wrapper.js';
+      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
 
       window.React = React;
 
@@ -25,71 +25,35 @@
   import Component0 from '../src/components/FilterBox.js';
 reactComponents['FilterBox'] = Component0;
 
-import Component1 from '../src/components/FilterBox.js';
-reactComponents['FilterBox'] = Component1;
+import Component1 from '../src/components/Map/Marker.js';
+reactComponents['Marker'] = Component1;
 
-import Component2 from '../src/components/Map/Marker.js';
-reactComponents['Marker'] = Component2;
+import Component2 from '../src/components/ListingDetails/ImageCarousel.js';
+reactComponents['ImageCarousel'] = Component2;
 
-import Component3 from '../src/components/Map/Marker.js';
-reactComponents['Marker'] = Component3;
+import Component3 from '../src/components/ListingDetails/ListingDetails.js';
+reactComponents['ListingDetails'] = Component3;
 
-import Component4 from '../src/components/ListingDetails/ImageCarousel.js';
-reactComponents['ImageCarousel'] = Component4;
+import Component4 from '../src/components/ListingDetails/ListingInfoItem.js';
+reactComponents['ListingInfoItem'] = Component4;
 
-import Component5 from '../src/components/ListingDetails/ImageCarousel.js';
-reactComponents['ImageCarousel'] = Component5;
+import Component5 from '../src/components/Map/Map.js';
+reactComponents['Map'] = Component5;
 
-import Component6 from '../src/components/ListingDetails/ListingDetails.js';
-reactComponents['ListingDetails'] = Component6;
+import Component6 from '../src/components/Map/Marker.js';
+reactComponents['Marker'] = Component6;
 
-import Component7 from '../src/components/ListingDetails/ListingDetails.js';
-reactComponents['ListingDetails'] = Component7;
+import Component7 from '../src/components/FilterItems/RangeSlider.js';
+reactComponents['RangeSlider'] = Component7;
 
-import Component8 from '../src/components/ListingDetails/ListingInfoItem.js';
-reactComponents['ListingInfoItem'] = Component8;
+import Component8 from '../src/components/ReviewModal.js';
+reactComponents['ReviewModal'] = Component8;
 
-import Component9 from '../src/components/ListingDetails/ListingInfoItem.js';
-reactComponents['ListingInfoItem'] = Component9;
+import Component9 from '../src/components/Searchbar.js';
+reactComponents['Searchbar'] = Component9;
 
-import Component10 from '../src/components/Map/Map.js';
-reactComponents['Map'] = Component10;
+import Component10 from '../src/components/FilterItems/SimpleSelect.js';
+reactComponents['SimpleSelect'] = Component10;
 
-import Component11 from '../src/components/Map/Map.js';
-reactComponents['Map'] = Component11;
-
-import Component12 from '../src/components/Map/Marker.js';
-reactComponents['Marker'] = Component12;
-
-import Component13 from '../src/components/Map/Marker.js';
-reactComponents['Marker'] = Component13;
-
-import Component14 from '../src/components/FilterItems/RangeSlider.js';
-reactComponents['RangeSlider'] = Component14;
-
-import Component15 from '../src/components/FilterItems/RangeSlider.js';
-reactComponents['RangeSlider'] = Component15;
-
-import Component16 from '../src/components/ReviewModal.js';
-reactComponents['ReviewModal'] = Component16;
-
-import Component17 from '../src/components/ReviewModal.js';
-reactComponents['ReviewModal'] = Component17;
-
-import Component18 from '../src/components/Searchbar.js';
-reactComponents['Searchbar'] = Component18;
-
-import Component19 from '../src/components/Searchbar.js';
-reactComponents['Searchbar'] = Component19;
-
-import Component20 from '../src/components/FilterItems/SimpleSelect.js';
-reactComponents['SimpleSelect'] = Component20;
-
-import Component21 from '../src/components/FilterItems/SimpleSelect.js';
-reactComponents['SimpleSelect'] = Component21;
-
-import Component22 from '../src/components/FilterItems/SimpleSlider.js';
-reactComponents['SimpleSlider'] = Component22;
-
-import Component23 from '../src/components/FilterItems/SimpleSlider.js';
-reactComponents['SimpleSlider'] = Component23;
+import Component11 from '../src/components/FilterItems/SimpleSlider.js';
+reactComponents['SimpleSlider'] = Component11;
